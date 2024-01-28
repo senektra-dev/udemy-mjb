0191556f75f05b5bf7b233f245bde2df596dcd6b
senektra <rcvallada@gmail.com>
2024-01-28 12:08:51 -0800

    Finish Timer class

--- section-20/index.js
+++ section-20/index.js
@@ -3,12 +3,38 @@ class Timer {
     this.durationInput = durationInput
     this.startButton = startButton
     this.pauseButton = pauseButton
+    this.intervalId = null
 
     this.startButton.addEventListener('click', this.start)
+    this.pauseButton.addEventListener('click', this.pause)
   }
 
-  start () {
-    console.log('Time to start the timer!')
+  start = () => {
+    if (!this.intervalId && this.timeRemaining) {
+      this.tick()
+      this.intervalId = setInterval(this.tick, 1000)
+    }
+  }
+
+  pause = () => {
+    clearInterval(this.intervalId)
+    this.intervalId = null
+  }
+
+  tick = () => {
+    if (this.timeRemaining) {
+      this.timeRemaining = this.timeRemaining - 1
+    } else {
+      this.pause()
+    }
+  }
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value)
+  }
+
+  set timeRemaining(val) {
+    this.durationInput.value = val
   }
 }
 
@@ -16,4 +42,4 @@ const durationInput = document.querySelector('#duration')
 const startButton = document.querySelector('#start')
 const pauseButton = document.querySelector('#pause')
 
-const timer = new Timer (durationInput, startButton, pauseButton)
+const timer = new Timer(durationInput, startButton, pauseButton)
